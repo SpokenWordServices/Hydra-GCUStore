89ec972374a3634715d3771758e212c5600a2a38
Dan Weinand <dweinand@gmail.com>
2009-10-30 01:07:37 -0500

    Result Grouping first pass.

--- app/helpers/salt_helper.rb
+++ app/helpers/salt_helper.rb
@@ -17,4 +17,31 @@ module SaltHelper
     link_to(name, catalog_index_path(facet_params), options)
   end
   
+  def grouped_result_count(response, facet_name=nil, facet_value=nil)
+    if facet_name && facet_value
+      facet = response.facets.detect {|f| f.name == facet_name}
+      facet_item = facet.items.detect {|i| i.value == facet_value} if facet
+      count = facet_item ? facet_item.hits : 0
+    else
+      count = response.docs.total
+    end
+    pluralize(count, 'document')
+  end
+  
+  def grouping_facet
+    fields = Hash[sort_fields]
+    case h(params[:sort])
+    when fields['date -']
+      'year_facet'
+    when fields['date +']
+      'year_facet'
+    when fields['document type']
+      'medium_t'
+    when fields['location']
+      'series_facet'
+    else
+      nil
+    end
+  end
+  
 end
--- vendor/plugins/blacklight/app/controllers/application_controller.rb
+++ vendor/plugins/blacklight/app/controllers/application_controller.rb
@@ -6,6 +6,8 @@ class ApplicationController < ActionController::Base
   
   filter_parameter_logging :password, :password_confirmation  
   helper_method :current_user_session, :current_user
+  
+  helper :all
 
   def user_class; User; end
   
