580e72f4e266500c72119ec83153b08011c1258e
McClain Looney <m@loonsoft.com>
2009-11-11 21:52:21 -0600

    It's the role mapper!
    And it's as big as a whale!
    
    Define your roles as a hash containing roles as keys pointing to
    arrays of usernames (as defined in webauth_user for instance)
    
    see config/roles_test.yml for an example
    
    See app models/role_mapper.rb for api

---
+++ spec/models/role_mapper_spec.rb
@@ -0,0 +1,18 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe RoleMapper do
+  
+ it "should quer[iy]able for roles for a given user" do
+   RoleMapper.roles('alice').should == ['archivist', 'donor']
+   RoleMapper.roles('bob').should == ['archivist']
+ end
+
+ it "should return an empty array if there are no roles" do
+   RoleMapper.roles('Marduk, the sun god').empty?.should == true
+ end
+ it "should know who is what" do
+   RoleMapper.whois('archivist').sort.should == %w(alice bob charlie)
+   RoleMapper.whois('stimutax salesman').empty?.should == true
+ end
+
+end
