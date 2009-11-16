5d5fac7b63cefc9017c71dbb1e269f24521daa05
Matt Zumwalt <matt.zumwalt@yourmediashelf.com>
2009-11-15 23:19:15 -0600

    working on metadata helpers

--- app/helpers/metadata_helper.rb
+++ app/helpers/metadata_helper.rb
@@ -12,14 +12,14 @@ module MetadataHelper
   # Convenience method for creating editable metadata fields.  Defaults to creating multi-value field, but creates single-value field if :multiple => false
   def editable_metadata_field(resource, datastream_name, field_name, opts={})    
     if opts[:multiple] == false
-      result = metadata_form_field(resource, datastream_name, field_name, opts)
+      result = metadata_inline_edit(resource, datastream_name, field_name, opts)
     else
-      result = metadata_form_field(resource, datastream_name, field_name, opts)
+      result = editable_multi_value_field(resource, datastream_name, field_name, opts)
     end
     return result
   end
    
-  def metadata_form_field(resource, datastream_name, field_name, opts={})
+  def metadata_inline_edit(resource, datastream_name, field_name, opts={})
     if opts.has_key?(:label) 
       label = opts[:label]
     else
@@ -27,9 +27,65 @@ module MetadataHelper
     end
     resource_type = resource.class.to_s.underscore
     
-    result = "<label for=\"#{resource_type}_#{field_name}\">#{label}</label>"    
-    result << "<span class=\"editableText\" id=\"#{resource_type}_#{field_name}\">#{get_values_from_datastream(resource, datastream_name, field_name).first}</span>"
+    result = "<dt for=\"#{resource_type}_#{field_name}\">#{label}</dt>"    
+    result << "<dd><span class=\"editableText\" id=\"#{resource_type}_#{field_name}\">#{get_values_from_datastream(resource, datastream_name, field_name).first}</span></dd>"
     return result
+  end
+  
+  # Creates an editable field that allows addition of an arbitrary number of field values.
+  def editable_multi_value_field(resource, datastream_name, field_key, opts={})
+    field_name=field_key.to_s
+    opts = {:ftype=>'field', :itype=>'editabletf'}.merge(opts)
+    label = opts[:label] ?  opts[:label] : field_name.humanize.capitalize
+    oid = resource.pid
+    rel = url_for(:action=>"show")
+    resource_type = resource.class.to_s.underscore
+    fmt = "#{resource_type}_%s_%s_%d"
+    fn=field_name.gsub(/_/, '+')
+    new_element_id = fmt%[oid, fn, -1]
+    
+    result = ""
+    result << "<dt id=\"#{field_name}\", class=\"field\">"
+    result << label
+    result << link_to_function("+" , "addLink()", :class=>'addmlink')
+    result << "</dt>"
+    
+    #Output all of the current field values.
+    datastream = resource.datastreams[datastream_name]
+    vlist = datastream.fields[field_key][:values] || []
+    vlist.each_with_index do |val,z|
+      result << "<dd, id=\"#{fn}_#{z}\">" 
+      result << link_to_remote(image_tag("delete.png"), :update => "", :url => {:action=>:show, "#{resource_type}[#{fn}][#{z}]"=>""}, :method => :put, :success => visual_effect(:fade, "#{fn}_#{z}"),:html => { :class  => "destructive" })
+      result << "<span class=\"editableText values #{opts[:itype]}\" id=\"#{resource_type}_#{fn}_#{z}\">"
+      result << ((val.blank? ?  "enter value" : h(val)))
+      result << "</span>"
+    end    
+    
+    return result
+    # haml_tag(:div, {:id=>"#{field_name}", :class=>"field"}) do
+    #   # Output the label and the + button for adding new values.
+    #   haml_tag :label do
+    #     puts "#{label}:"
+    #     puts(link_to_function("+" , nil, :class=>'addmlink') do |page|
+    #       #page.insert_html(:bottom, field_name,  "<div class=\"values #{opts[:itype]}\" id=\"#{fmt%[oid, fn, -1]}\" rel=\"#{rel}\">New</div>")
+    #       #page << "Editable.create($('#{fmt%[oid, fn, -1]}'))"
+    #       page.insert_html(:bottom, field_name, render(:partial=>"shared/new_field_value_form", :locals=>{:field_name=>fn,:oid=>oid,:resource_type=>resource_type,:rel=>rel, :element_id=>new_element_id}) )
+    #       page << "Editable.create_active($('#{new_element_id}'))"
+    #     end)
+    #   end
+    #   
+    #   # Output all of the current field values.
+    #   datastream = resource.datastreams[datastream_name]
+    #   vlist = datastream.fields[field_key][:values] || []
+    #   vlist.each_with_index do |val,z|
+    #     haml_tag(:div, :id=>"#{fn}_#{z}") do
+    #       puts link_to_remote(image_tag("delete.png"), :update => "", :url => {:action=>:show, "#{resource_type}[#{fn}][#{z}]"=>""}, :method => :put, :success => visual_effect(:fade, "#{fn}_#{z}"),:html => { :class  => "destructive" })
+    #       haml_tag(:div, {:class=>"values #{opts[:itype]}",:id=>fmt%[oid, fn, z], :rel=>rel}) do
+    #         puts((val.blank? ?  "enter value" : h(val)))
+    #       end
+    #     end
+    #   end 
+    # end
   end
   
   def get_values_from_datastream(resource, datastream_name, field_name)
