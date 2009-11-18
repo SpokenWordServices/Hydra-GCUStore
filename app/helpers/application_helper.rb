734006aa989c8b4742eba89bfb76f33e922694fa
Matt Zumwalt <matt.zumwalt@yourmediashelf.com>
2009-11-18 17:56:16 -0600

    minor - moved a helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,17 +7,4 @@ module ApplicationHelper
     'SALT (Self Archiving Legacy Toolkit)'
   end
   
-  def edit_and_browse_links
-    result = ""
-    if params[:action] == "edit"
-      result << "<a href=\"#{catalog_path(@document[:id])}\" class=\"browse\">Browse</a>"
-      result << "<a href=\"#\" class=\"edit active\">Edit</a>"
-    else
-      result << "<a href=\"#\" class=\"browse active\">Browse</a>"
-      result << "<a href=\"#{edit_catalog_path(@document[:id])}\" class=\"edit\">Edit</a>"
-    end
-    # result << link_to "Browse", "#", :class=>"browse"
-    # result << link_to "Edit", edit_document_path(@document[:id]), :class=>"edit"
-    return result
-  end
 end
--- app/helpers/salt_helper.rb
+++ app/helpers/salt_helper.rb
@@ -25,6 +25,19 @@ module SaltHelper
     link_to(name, catalog_index_path(facet_params), options)
   end
   
+  def edit_and_browse_links
+    result = ""
+    if params[:action] == "edit"
+      result << "<a href=\"#{catalog_path(@document[:id])}\" class=\"browse\">Browse</a>"
+      result << "<a href=\"#\" class=\"edit active\">Edit</a>"
+    else
+      result << "<a href=\"#\" class=\"browse active\">Browse</a>"
+      result << "<a href=\"#{edit_catalog_path(@document[:id])}\" class=\"edit\">Edit</a>"
+    end
+    # result << link_to "Browse", "#", :class=>"browse"
+    # result << link_to "Edit", edit_document_path(@document[:id]), :class=>"edit"
+    return result
+  end
   
   def collection_title
     "Edward A. Feigenbaum Papers"
