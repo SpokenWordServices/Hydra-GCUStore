a4a83ea1172c9a7257c4732390639cb438af3390
Matt Zumwalt <matt.zumwalt@yourmediashelf.com>
2009-11-20 21:02:24 -0600

    minor

--- public/javascripts/custom.js
+++ public/javascripts/custom.js
@@ -3,14 +3,4 @@ function async_load(url, divid) {
   return null;
 }
 
-// function async_load_prototype(url, divid) {
-//  new Ajax.Request(url,
-//    {
-//      method:'get',
-//      onSuccess: function(transport){
-//      $(divid).update(transport.responseText);
-//          alert("Success! \n\n" + response);
-//      },
-//      onFailure: function(){ alert('Something went wrong loading ' + url) }
-//    });
-// }
+function
