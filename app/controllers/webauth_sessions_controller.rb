ed332c512db92b589f104495df0dd9345555c0ed
McClain Looney <mlooney@mediashelf.us>
2010-01-22 13:19:59 -0800

    unser session state on logout before redirecting to sso logout

--- app/controllers/webauth_sessions_controller.rb
+++ app/controllers/webauth_sessions_controller.rb
@@ -1,9 +1,11 @@
 class WebauthSessionsController < ApplicationController
   skip_before_filter :store_bounce
   def new
-    redirect_to session[:bounce]
+    redirect_to session[:bounce] if session[:bounce]
   end
   def destroy
+    session[:bounce]=nil
+    session[:user]=nil
     redirect_to 'https://weblogin.stanford.edu/logout'
   end
 end
