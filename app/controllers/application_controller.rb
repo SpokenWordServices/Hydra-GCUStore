4109c81bcb763c0a7ad041f41e505d94c288b570
Matt Zumwalt <matt.zumwalt@yourmediashelf.com>
2010-05-09 18:11:58 -0500

    stripped back application_controller.rb in the root application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,8 +12,6 @@ class ApplicationController < ActionController::Base
   
   helper :all
   helper :hydra_access_controls, :hydra_djatoka, :downloads, :metadata, :hydra
-
-  def user_class; User; end
   
   helper_method [:request_is_for_user_resource?]#, :user_logged_in?]
   before_filter [:set_current_user, :store_bounce]
@@ -22,46 +20,9 @@ class ApplicationController < ActionController::Base
   # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery # :secret => '200c1e5f25e610288439b479ef176bbd'
   
-  layout :choose_layout
-  
-  # test for exception notifier plugin
-  def error
-    raise RuntimeError, "Generating a test error..."
-  end
-  
   protected
   def store_bounce 
     session[:bounce]=params[:bounce]
   end
-  
-    # Returns a list of Searches from the ids in the user's history.
-    def searches_from_history
-      session[:history].blank? ? [] : Search.find(session[:history]) rescue []
-    end
-    
-    #
-    # Controller and view helper for determining if the current url is a request for a user resource
-    #
-    def request_is_for_user_resource?
-      request.env['PATH_INFO'] =~ /\/?users\/?/
-    end
-    
-    #
-    # If a param[:no_layout] is set OR
-    # request.env['HTTP_X_REQUESTED_WITH']=='XMLHttpRequest'
-    # don't use a layout, otherwise use the "application.html.erb" layout
-    #
-    def choose_layout
-      'application' unless request.xml_http_request? || ! params[:no_layout].blank?
-    end
-    
-    def current_user_session
-      return @current_user_session if defined?(@current_user_session)
-      @current_user_session = UserSession.find
-    end
 
-    def current_user
-      return @current_user if defined?(@current_user)
-      @current_user = current_user_session && current_user_session.user
-    end
 end
