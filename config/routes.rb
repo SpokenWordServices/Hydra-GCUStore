bbcb405e9605d4fcce45e4aa3f79ed75f7994c64
Matt Zumwalt <matt.zumwalt@yourmediashelf.com>
2010-05-13 00:27:36 -0500

    adding FileAssets views and routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,9 @@ ActionController::Routing::Routes.draw do |map|
   map.edit_catalog 'catalog/:id/edit', :controller=>:catalog, :action=>:edit
   
   #map.root :controller => 'collections', :action=>'index'
-  map.resources :assets do |assets|
-    assets.resources :downloads, :only=>[:index]
-  end
+  # map.resources :assets do |assets|
+  #   assets.resources :downloads, :only=>[:index]
+  # end
   map.resources :get, :only=>:show  
   map.resources :webauths, :protocol => ((defined?(SSL_ENABLED) and SSL_ENABLED) ? 'https' : 'http')
   map.login "login", :controller => "webauth_sessions", :action => "new"
