a2062db0d9547d204305cc61b59b534a8304cdf3
Edwin Shin <edwin.shin@yourmediashelf.com>
2010-05-16 23:29:45 +0200

    added dependency for sqlite3-ruby

--- config/environment.rb
+++ config/environment.rb
@@ -30,6 +30,7 @@ Rails::Initializer.run do |config|
   config.gem 'solrizer', :version=>">= 0.0.9"
   config.gem 'authlogic', :version=>">= 0.1.0"
   config.gem "haml"
+  config.gem 'sqlite3-ruby', :lib => 'sqlite3'
   config.plugin_paths += ["#{RAILS_ROOT}/vendor/plugins/blacklight/vendor/plugins"]  
   config.plugins = %W(engines blacklight acts_as_taggable_on_steroids resource_controller haml fluid-infusion hydra_repository stanford_salt white_list)
   
