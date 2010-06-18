046e59e7c1c56bf1b47af88ad4b08e4197a09fc1
jchristo4 <jchristo4@gmail.com>
2010-06-17 18:10:09 -0700

    Added split button for create asset function (#949)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,7 +22,7 @@ class ApplicationController
   
   
   def default_html_head
-    javascript_includes << ['http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js', 'http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.1/jquery-ui.min.js']
+    javascript_includes << ['http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js', 'http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.1/jquery-ui.min.js', 'application']
     javascript_includes << ['blacklight', 'application', 'accordion', { :plugin=>:blacklight } ]
     
     stylesheet_links << ['yui', 'jquery/ui-lightness/jquery-ui-1.8.1.custom.css', 'application', {:plugin=>:blacklight, :media=>'all'}]
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,27 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+$(function() {
+  $("#re-run-action").next().button( {
+    text: false,
+    icons: { primary: "ui-icon-triangle-1-s" }
+  })
+  .click(function() {
+    $('#create-asset-menu').is(":hidden") ? 
+      $('#create-asset-menu').show() : $('#create-asset-menu').hide();
+    })
+  .parent().buttonset();
+  
+  $('#create-asset-menu').mouseleave(function(){
+    $('#create-asset-menu').hide();
+  });
+});
+
+function navigateTo(elem, link) {
+  $('#re-run-action').attr('value', $(elem).text())
+  .click(function() {
+    $('#create-asset-menu').hide();
+    location.href = link;
+  }).click();
+}
+
