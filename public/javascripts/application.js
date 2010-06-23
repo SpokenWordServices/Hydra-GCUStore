a21195a29d47e9a47a97c3b3c75a1603d66ecd84
jchristo4 <jchristo4@gmail.com>
2010-06-22 22:43:09 -0700

    Completed fixes to create-asset split button (#949)

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,6 +2,9 @@
 // This file is automatically included by javascript_include_tag :defaults
 
 $(function() {
+  var elemFirst = $('ul#create-asset-menu li:first-child');	
+  setupNavigation(elemFirst, elemFirst.text());
+	
   $("#re-run-action").next().button( {
     text: false,
     icons: { primary: "ui-icon-triangle-1-s" }
@@ -14,14 +17,21 @@ $(function() {
   
   $('#create-asset-menu').mouseleave(function(){
     $('#create-asset-menu').hide();
-  });
+  })
+  .css('top', $('re-run-action').height());
 });
 
-function navigateTo(elem, link) {
-  $('#re-run-action').attr('value', $(elem).text())
+function setupNavigation(elem, link) {
+  $('#re-run-action')
+  .attr('value', $(elem).text())
   .click(function() {
     $('#create-asset-menu').hide();
     location.href = link;
-  }).click();
+  });
+}
+
+function navigateTo(elem, link) {
+  setupNavigation(elem, link);	
+  $('#re-run-action').click();	
 }
 
