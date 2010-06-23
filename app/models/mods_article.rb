3b3cb36bb3ed94a5c215563a600cf149c0c3847c
Matt Zumwalt <matt.zumwalt@yourmediashelf.com>
2010-06-23 18:45:06 -0500

    real hydra rights metadata datastream; restructuring edit_permissions to match mockups & to work with jquery slider

--- app/models/mods_article.rb
+++ app/models/mods_article.rb
@@ -4,26 +4,13 @@ class ModsArticle < ActiveFedora::Base
   
   has_relationship "parts", :is_part_of, :inbound => true
   
-  # simpleRightsMetadata datastream is a stand-in for the rightsMetadata datastream that will eventually have Hydra Rights Metadata xml in it
-  # It will eventually be declared with this one-liner:
-  # has_metadata :name => "rightsMetadata", :type => Hydra::RightsMetadataDatastream
+  # Uses the Hydra Rights Metadata Schema for tracking access permissions & copyright
+  has_metadata :name => "rightsMetadata", :type => Hydra::RightsMetadata 
   
-  has_metadata :name => "rightsMetadata", :type => ActiveFedora::MetadataDatastream do |m|
-    m.field "discover_access_group", :string
-    m.field "read_access_group", :string
-    m.field "edit_access_group", :string
-    
-    m.field "discover_access", :string
-    m.field "read_access", :string
-    m.field "edit_access", :string
-  end
-  
-  # modsMetadata datastream is a stand-in for the descMetadata datastream that will eventually have MODS metadata in it
-  # It will eventually be declared with this one-liner:
-  # has_metadata :name => "descMetadata", :type => Hydra::ModsDatastream 
-  
+  # Uses the Hydra MODS Article profile for tracking most of the descriptive metadata
   has_metadata :name => "descMetadata", :type => Hydra::ModsArticle 
 
+  # A place to put extra metadata values
   has_metadata :name => "properties", :type => ActiveFedora::MetadataDatastream do |m|
     m.field 'collection', :string
   end
