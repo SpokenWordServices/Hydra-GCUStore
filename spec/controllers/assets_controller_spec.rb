422165a611ddcc0d46a416115eb01d7c77253f45
Matt Zumwalt <matt.zumwalt@yourmediashelf.com>
2010-06-25 19:19:29 -0500

    updating AssetsController.update api

--- spec/controllers/assets_controller_spec.rb
+++ spec/controllers/assets_controller_spec.rb
@@ -23,19 +23,44 @@ describe AssetsController do
       mock_document = mock("document")
       HydrangeaArticle.expects(:find).with("_PID_").returns(mock_document)
       
-      mock_document.expects(:update_indexed_attributes).with({"subject"=>{"-1"=>"My Topic"}}, {}).returns({"subject"=>{"2"=>"My Topic"}})
+      simple_request_params = {"asset"=>{
+          "descMetadata"=>{
+            "subject"=>{"0"=>"subject1", "1"=>"subject2", "2"=>"subject3"}
+          }
+        }
+      }
+      
+      mock_document.expects(:update_indexed_attributes).with({"subject"=>{"0"=>"subject1", "1"=>"subject2", "2"=>"subject3"}}, {:datastreams=>"descMetadata"}).returns({"subject"=>{"2"=>"My Topic"}})
       mock_document.expects(:save)
-      put :update, :id=>"_PID_", "asset"=>{"subject"=>{"-1"=>"My Topic"}}
-      #session[:user].should == 'bob'
+      # put :update, :id=>"_PID_", "asset"=>{"subject"=>{"-1"=>"My Topic"}}
+      put :update, {:id=>"_PID_"}.merge(simple_request_params)
     end
+    
     it "should support updating OM::XML datastreams" do
       mock_document = mock("document")
       HydrangeaArticle.expects(:find).with("_PID_").returns(mock_document)
       
-      update_method_args = [ {:parent_select=>[{:person=>0}, :last_name], :child_index=>0, :values=>"Sample New Value"}, {:datastreams=>"descMetadata"} ]
-      mock_document.expects(:update_indexed_attributes).with( *update_method_args ).returns({"person_0_last_name"=>{"0"=>"Sample New Value"}})
+      update_method_args = [ { [{:person=>0}, :role] => {"0"=>"role1","1"=>"role2","2"=>"role3"} }, {:datastreams=>"descMetadata"} ]
+      mock_document.expects(:update_indexed_attributes).with( *update_method_args ).returns({"person_0_role"=>{"0"=>"role1","1"=>"role2","2"=>"role3"}})
       mock_document.expects(:save)
-      put :update, :id=>"_PID_", "content_type"=>"hydrangea_article", "datastream"=>"descMetadata", "field_name"=>"person_0_last_name","parent_select"=>[{":person"=>"0"}, ":last_name"], "child_index"=>"0", "value"=>"Sample New Value"
+      
+      
+      nokogiri_request_params = {
+        "id"=>"_PID_", 
+        "content_type"=>"hydrangea_article",
+        "field_selectors"=>{
+          "descMetadata"=>{
+            "person_0_role"=>[{":person"=>"0"}, "role"]
+          }
+        }, 
+        "asset"=>{
+          "descMetadata"=>{
+            "person_0_role"=>{"0"=>"role1", "1"=>"role2", "2"=>"role3"}
+          }
+        }
+      }
+      put :update, nokogiri_request_params
+      # put :update, :id=>"_PID_", "content_type"=>"hydrangea_article", "datastream"=>"descMetadata", "field_name"=>"person_0_last_name","parent_select"=>[{":person"=>"0"}, ":last_name"], "child_index"=>"0", "value"=>"Sample New Value"
     end
     it "should initialize solr" do
       pending
