22a56e2f535b5db123d26d2b952906b9b4055415
Paul Wenzel <paul@co-opmedia.org>
2010-08-06 00:26:55 -0500

    replaced jquery ui accordion with simpler version, fixing the one open at a time issue (bug #1067)

--- public/javascripts/custom.js
+++ public/javascripts/custom.js
@@ -40,19 +40,51 @@ Hydrangea.FileUploader = function() {
 // Accordion Behavior
 jQuery(document).ready(function($) {
 
-    // activate accordion behavior for all accordion-section elements
-    $(".accordion-section").accordion({
-        autoHeight: false,
-        clearStyle: false,
-        collapsible: true,
-        active: false,
-        icons: false
-    });
+	// Replaced cumbersome jQuery UI Accordion with this much simpler version (Paul Wenzel)
+	
+    $('.accordion-section').children(":not(.section-title)").hide();
+	
+    $('.accordion-section .section-title').click(function(){
+		if($(this).hasClass("active")) {
+			
+		} else {
+			$(".active").siblings().hide()
+			$(".active").toggleClass("active");
+			$(this).siblings().toggle();
+			$(this).addClass("active");
+		}
 
+     });
+
+	$('.accordion-section .section-title:first').click();
+	$('.accordion-section .section-title:first').addClass("active");
+
+
+/*
+	// activate accordion behavior for all accordion-section elements
+	$(".accordion-section").accordion({
+	    autoHeight: false,
+	    clearStyle: false,
+	    collapsible: true,
+	    active: false,
+	    icons: false
+	}).bind("accordionchangestart",
+	function(event, ui) {
+		console.log('accordionchangestart triggered');
+		$(".activeAccordion").removeClass("activeAccordion");
+		
+	}).bind("accordionchange",
+	function(event, ui) {
+		console.log('accordionchange triggered');
+		// $(this).addClass("activeAccordion");
+	});
+	
     // open up the first accordion-section by "clicking"
     $(".accordion-section:first h2").click();
-
+	$(".accordion-section:first").addClass("activeAccordion");
+*/
     // Disable styles inherited by jQuery UI (see also bottom of public/plugin_assets/blacklight/stylesheets/jquery/ui-lightness/jquery-ui-1.8.1.custom.css)
+	
     $(".ui-accordion-content").removeClass("ui-accordion ui-widget ui-accordion-content ui-helper-reset ui-widget-content ui-corner-bottom ui-accordion-content-active");
     $(".accordion-section h2").removeClass("ui-accordion-header ui-helper-reset ui-state-active ui-corner-top ui-state-default ui-corner-all").css('cursor', 'pointer');
     $("div.ui-widget").css({
