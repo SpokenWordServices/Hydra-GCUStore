0ac6a0211a77d3096bfd2ba7dcbea202ea7ab605
John Scofield <jscofield@profitstreams.com>
2010-08-29 00:24:58 -0600

    Consolidating code into vendor/plugins directory.

--- vendor/plugins/hydra_repository/app/helpers/application_helper.rb
+++ vendor/plugins/hydra_repository/app/helpers/application_helper.rb
@@ -44,4 +44,30 @@ module ApplicationHelper
     
     link_to_with_data(label, catalog_path(doc[:id]), {:method => :put, :data => {:counter => opts[:counter]},:title=>opts[:title]})
   end
+
+  # currently only used by the render_document_partial helper method (below)
+  def document_partial_name(document)
+    if !document[Blacklight.config[:show][:display_type]].nil?
+      return document[Blacklight.config[:show][:display_type]].first.gsub("info:fedora/afmodel:","").underscore.pluralize
+    else
+      return nil
+    end
+  end
+  
+  # Overriding Blacklight's render_document_partial
+  # given a doc and action_name, this method attempts to render a partial template
+  # based on the value of doc[:format]
+  # if this value is blank (nil/empty) the "default" is used
+  # if the partial is not found, the "default" partial is rendered instead
+  def render_document_partial(doc, action_name, locals={})
+    format = document_partial_name(doc)
+    begin
+      Rails.logger.debug("attempting to render #{format}/_#{action_name}")
+      render :partial=>"#{format}/#{action_name}", :locals=>{:document=>doc}.merge(locals)
+    rescue ActionView::MissingTemplate
+      Rails.logger.debug("rendering default partial catalog/_#{action_name}_partials/default")
+      render :partial=>"catalog/_#{action_name}_partials/default", :locals=>{:document=>doc}.merge(locals)
+    end
+  end
+
 end
