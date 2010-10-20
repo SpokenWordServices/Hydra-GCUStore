69be9dc68f8fc98eac7afe17d7467c512abb3192
Matt Zumwalt <matt.zumwalt@yourmediashelf.com>
2010-10-20 11:41:36 -0500

    model-level convenience methods for grants

--- vendor/plugins/hydrangea_datasets/app/models/hydrangea_dataset.rb
+++ vendor/plugins/hydrangea_datasets/app/models/hydrangea_dataset.rb
@@ -21,4 +21,11 @@ class HydrangeaDataset < ActiveFedora::Base
   # A place to put extra extra metadata values
   has_metadata :name => "hydraDataset", :type => HydraDatasetDs
   
+  # Call insert_contributor on the descMetadata datastream
+  def insert_grant
+    ds = self.datastreams_in_memory["hydraDataset"]   
+    node, index = ds.insert_grant
+    return node, index
+  end
+  
 end
