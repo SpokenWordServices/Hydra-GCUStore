14c3bcdfc23a168b03e7897b697120f5b826c696
Garrick Van Buren <garrickvanburen@Garrick-Van-Burens-MacBook-Pro.local>
2010-11-02 15:08:11 -0500

    'add a [contributor]' buttons now remember the last contributor type clicked, HYDRA-164

--- public/javascripts/jquery.hydraMetadata.js
+++ public/javascripts/jquery.hydraMetadata.js
@@ -90,7 +90,6 @@
      addContributor: function(type) {
        var content_type = $("form#new_contributor > input#content_type").first().attr("value");
        var contributors_group_selector = "."+type+".contributor";
-
        var url = $("form#new_contributor").attr("action");
 
        $.post(url, {contributor_type: type, content_type: content_type},function(data) {
@@ -98,6 +97,7 @@
          $inserted = $(contributors_group_selector).last();
          $(".editable-container", $inserted).hydraTextField();
          $("a.destructive", $inserted).hydraContributorDeleteButton();
+				$("#re-run-add-contributor-action").val("Add a " + type);
        });
      },
      
