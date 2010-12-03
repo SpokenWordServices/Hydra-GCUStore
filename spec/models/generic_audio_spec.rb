d6b169f3c2b3a8a47d9edcea23fe49db0ac77999
Graeme West <graeme@heliocentrik.net>
2010-12-03 16:28:53 +0000

    oops forgot GenericAudio spec file

--- spec/models/generic_audio_spec.rb
+++ spec/models/generic_audio_spec.rb
@@ -5,23 +5,23 @@ describe GenericAudio do
   
   before(:each) do
     Fedora::Repository.stubs(:instance).returns(stub_everything())
-    @hydra_image = GenericAudio.new
+    @hydra_audio = GenericAudio.new
 
   end
   
   it "Should be a kind of ActiveFedora::Base" do
-    @hydra_image.should be_kind_of(ActiveFedora::Base)
+    @hydra_audio.should be_kind_of(ActiveFedora::Base)
   end
   
   it "should include Hydra Model Methods" do
-    @hydra_image.class.included_modules.should include(Hydra::ModelMethods)
-    @hydra_image.should respond_to(:apply_depositor_metadata)
+    @hydra_audio.class.included_modules.should include(Hydra::ModelMethods)
+    @hydra_audio.should respond_to(:apply_depositor_metadata)
   end
   
   it "should have accessors for its default content datastream" do
-    @hydra_image.should respond_to(:has_content?)
-    @hydra_image.should respond_to(:content)
-    @hydra_image.should respond_to(:content=)
+    @hydra_audio.should respond_to(:has_content?)
+    @hydra_audio.should respond_to(:content)
+    @hydra_audio.should respond_to(:content=)
   end
   
  
