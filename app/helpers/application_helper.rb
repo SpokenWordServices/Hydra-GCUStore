f33e12da8c92e95e7c469ee5c3956260e1e485d8
Bess Sadler <bess@stanford.edu>
2011-01-24 15:50:07 -0800

    If you don't require the application_helpers from vendor/plugins/blacklight and vendor/plugins/hydra_repository then those methods will not be available to the app

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,14 @@
+require 'vendor/plugins/blacklight/app/helpers/application_helper.rb'
+require 'vendor/plugins/hydra_repository/app/helpers/application_helper.rb'
 module ApplicationHelper
   
 # For some reason, this file seems to need to exists in order for rspec to work. 
 # If not, the ApplicationHelper from hydra_repository is not loaded  and (therefore) the Blacklight
 # ApplicationHelper is not overridden. 
 
-  
+  # Change the name of your application here
+  def application_name
+    'Hydrangea'
+  end
   
 end
