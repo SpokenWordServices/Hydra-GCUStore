b3ca439791d425db9e7340ff35e5529317588060
Matt Zumwalt <matt.zumwalt@yourmediashelf.com>
2011-02-24 17:56:29 -0600

    HYDRA-373 updating common_mods_index_methods and classes that rely on them

--- spec/lib/hydra_repository/common_mods_index_methods_spec.rb
+++ spec/lib/hydra_repository/common_mods_index_methods_spec.rb
@@ -6,19 +6,21 @@ describe Hydra::CommonModsIndexMethods do
   describe "extract_person_full_names" do
     it "should return an array of Solr::Field objects for :person_full_name_facet" do
       full_names = HydrangeaArticle.find("hydrangea:fixture_mods_article1").datastreams["descMetadata"].extract_person_full_names
-      full_names.should be_kind_of Array
-      full_names.length.should == 2
-      full_names.first.should be_kind_of Solr::Field
-      full_names.last.should be_kind_of Solr::Field
+      full_names.should be_kind_of Hash
+      full_names["person_full_name_facet"].should be_kind_of Array
+      full_names["person_full_name_facet"].length.should == 2
+      full_names["person_full_name_facet"].first.should == "FAMILY NAME, GIVEN NAMES"
+      full_names["person_full_name_facet"].last.should == "Lacks, Henrietta"
     end
   end
   describe "extract_person_organizations" do 
     it "should return an array of Solr::Field objects for :mods_organization_facet" do
       orgs = HydrangeaArticle.find("hydrangea:fixture_mods_article1").datastreams["descMetadata"].extract_person_organizations
-      orgs.should be_kind_of Array
-      orgs.length.should == 2
-      orgs.first.should be_kind_of Solr::Field
-      orgs.last.should be_kind_of Solr::Field
+      orgs.should be_kind_of Hash
+      orgs["mods_organization_facet"].should be_kind_of Array
+      orgs["mods_organization_facet"].length.should == 2
+      orgs["mods_organization_facet"].first.should == "FACULTY, UNIVERSITY"
+      orgs["mods_organization_facet"].last.should == "Baltimore"
     end
   end
 end
