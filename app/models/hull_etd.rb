340ea4f9890a4e5859f936bf6712312b1f95728b
John Scofield <john.scofield@yourmediashelf.com>
2011-03-09 23:06:53 -0500

    HYHULL-32: adding HullEtd model

---
+++ app/models/hull_etd.rb
@@ -0,0 +1,27 @@
+require "hydra"
+
+class HullEtd < ActiveFedora::Base
+  
+  include Hydra::ModelMethods
+  
+  has_relationship "parts", :is_part_of, :inbound => true
+  
+  # Uses the Hydra Rights Metadata Schema for tracking access permissions & copyright
+  has_metadata :name => "rightsMetadata", :type => Hydra::RightsMetadata 
+  
+  # Uses the Hydra MODS Article profile for tracking most of the descriptive metadata
+# TODO: define terminology for ETD
+  has_metadata :name => "descMetadata", :type => HullModsEtd
+
+  # A place to put extra metadata values
+  has_metadata :name => "properties", :type => ActiveFedora::MetadataDatastream do |m|
+    m.field 'collection', :string
+    m.field 'depositor', :string
+  end
+
+  def to_solr(solr_doc=Hash.new)
+    super(solr_doc)
+    solr_doc
+  end
+
+end
