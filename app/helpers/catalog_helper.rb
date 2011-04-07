82c8f60080e18b48531e0acebc6e37ee8c8dce46
simonlamb <s.lamb@hull.ac.uk>
2011-04-07 16:39:45 +0100

    HYHULL-JournalArticle:Updated Journal article to reflect latest descMetadata standard.  Moved _show_resources/links/downloads and index_views to the shared directory and referenced from content types where possible.

--- app/helpers/catalog_helper.rb
+++ app/helpers/catalog_helper.rb
@@ -2,7 +2,6 @@ module CatalogHelper
   include ActionView::Helpers::TextHelper
   require_dependency "vendor/plugins/hydra_repository/app/helpers/catalog_helper.rb"
 
-
   def get_persons_from_roles(doc,roles,opts={})
     i = 0
     persons =[]
@@ -10,7 +9,7 @@ module CatalogHelper
       persons_roles = [] # reset the array
       persons_roles = doc["person_#{i}_role_t"].map{|w|w.strip.downcase} unless doc["person_#{i}_role_t"].nil?
       if persons_roles and (persons_roles & roles).length > 0
-        persons << {:name => doc["person_#{i}_namePart_t"],:role => doc["person_#{i}_role_t"]}
+        persons << {:name => doc["person_#{i}_namePart_t"],:role => doc["person_#{i}_role_t"], :affiliation => doc["person_#{i}_affiliation_t"] }
       end
       i += 1
     end
