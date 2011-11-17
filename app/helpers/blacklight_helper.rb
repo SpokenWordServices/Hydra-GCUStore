8e7edbe7bc0b2a766cbb88a535c0ea5ea1b53614
Justin Coyne <justin.coyne@yourmediashelf.com>
2011-11-17 16:27:12 -0600

    Display the blue delete icon.  Fix the add new subject form

--- app/helpers/blacklight_helper.rb
+++ app/helpers/blacklight_helper.rb
@@ -1,7 +1,7 @@
 module BlacklightHelper
   include Hydra::BlacklightHelperBehavior
 
-  def render_facet_value(solr_field, item)
+  def render_facet_value(solr_field, item, options={})
     if solr_field == 'top_level_collection_id_s'
 		  link_to "Collections", "/?f%5Bis_member_of_s%5D%5B%5D=info:fedora/hull:rootDisplaySet&results_view=true" 
     else
