422c4c84f6e13c47b3f2625935e87ec6785a77c2
Justin Coyne <justin.coyne@yourmediashelf.com>
2011-12-05 11:38:52 -0600

    Wrote a failing test for HYHULL-502

--- config/initializers/active_fedora_overrides.rb
+++ config/initializers/active_fedora_overrides.rb
@@ -9,7 +9,7 @@ module ActiveFedora
     end
 
     def self.default_model(obj)
-      unless obj.datastreams['descMetadata'].nil? || obj.datastreams['rightsMetadata'].nil? || obj.datastreams['contentMetadata'].nil?
+      if obj.datastreams['descMetadata'] && obj.datastreams['rightsMetadata'] && obj.datastreams['contentMetadata']
         if ActiveFedora::Model.send(:class_exists?, "GenericContent")
           m = Kernel.const_get("GenericContent")
           if m
---
+++ spec/models/mods_journal_article_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe ModsJournalArticle do
+  describe "to_solr" do
+    before do 
+      @doc = ModsJournalArticle.from_xml(nil)
+      @doc.title_info.main_title = "Foo"
+      @doc.journal.title_info.main_title = "Bar"
+    end
+    it "should only have one title_facet in the solr doc" do
+      ### When a solr field has many terms, then solr can not use it for sort by 
+      @doc.title_info.main_title.should == ["Foo"]
+      @doc.to_solr['title_facet'].should == ["Foo"]
+      
+    end
+  end
+
+
+end
