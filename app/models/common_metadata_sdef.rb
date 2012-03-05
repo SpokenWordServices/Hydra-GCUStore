b277b60a8ac7312f5638db235c010b7060282e9d
simonlamb <s.lamb@hull.ac.uk>
2012-03-05 10:56:54 +0000

    HYHULL: Changed ServiceDef call to be more efficient

--- app/models/common_metadata_sdef.rb
+++ app/models/common_metadata_sdef.rb
@@ -1,6 +1,9 @@
 module CommonMetadataSdef
-	  def self.included(mod)
-		  mod.has_service_definition "hydra-sDef:commonMetadata"
-	  end
+	  def self.included(mod)		  
+      #mod.has_service_definition "hydra-sDef:commonMetadata"
+      #Deliberately calling the (Service_defintion) add_method direct
+      #  - This saves a call to fedora to get commonMetadata methods when we know we just want 'getOAI_DC' 
+      mod.add_method! "hydra-sDef:commonMetadata", "getOAI_DC"
+ 	  end
 end
 
