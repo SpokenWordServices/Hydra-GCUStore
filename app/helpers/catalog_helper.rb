def53638eae532a5de440aed7385a0c994c02969
Caroline Webb <caroline.webb@gcu.ac.uk>
2012-03-29 09:21:55 -0700

    First stab at jwplayer - doesn\' work, won\'t accept output from fedora

--- app/helpers/catalog_helper.rb
+++ app/helpers/catalog_helper.rb
@@ -304,5 +304,19 @@ module CatalogHelper
    resource_icon.html_safe
   end
 
+  # Helpers copied from Graeme's work - might need tweeking
+  def datastream_disseminator_url pid, datastream_name
+    begin
+      base_url = Fedora::Repository.instance.send(:connection).site.to_s
+    rescue
+      base_url = "http://localhost:8983/fedora"
+    end
+    "#{base_url}/get/#{pid}/#{datastream_name}"
+  end
+  
+  def disseminator_link pid, datastream_name
+    "<a class=\"fbImage\" href=\"#{ datastream_disseminator_url(pid, datastream_name) }\">view</a>"
+  end 
+
 end
 
