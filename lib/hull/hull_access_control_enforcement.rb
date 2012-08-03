74801b2d7269bffba1d88b13d6b9329607df2398
Caroline Webb <caroline.webb@gcu.ac.uk>
2012-08-03 08:50:07 -0700

    Override enforce_ show_permissions to properly reflect rightsMetadata

--- lib/hull/hull_access_control_enforcement.rb
+++ lib/hull/hull_access_control_enforcement.rb
@@ -18,5 +18,30 @@ module HullAccessControlEnforcement
  		end
 		false
 	end		  
+
+  # Redefine enforce_show_permissions as the hydra-head version seems to wrongly allow public through when should only be discovering not reading
+  def enforce_show_permissions(opts={})
+    load_permissions_from_solr
+#    unless @permissions_solr_document['access_t'] && (@permissions_solr_document['access_t'].first == "public" || @permissions_solr_document['access_t'].first == "Public")
+    unless @permissions_solr_document['access_t'] && (@permissions_solr_document['access_t'][1] =~ /\b(?i)public\b/ )
+      if @permissions_solr_document["embargo_release_date_dt"] 
+        embargo_date = Date.parse(@permissions_solr_document["embargo_release_date_dt"].split(/T/)[0])
+        if embargo_date > Date.parse(Time.now.to_s)
+          # check for depositor raise "#{@document["depositor_t"].first} --- #{user_key}"
+          ### Assuming we're using devise and have only one authentication key
+          unless current_user && user_key == @permissions_solr_document["depositor_t"].first
+            flash[:notice] = "This item is under embargo.  You do not have sufficient access privileges to read this document."
+            redirect_to(:action=>'index', :q=>nil, :f=>nil) and return false
+          end
+        end
+      end
+      unless reader?
+        flash[:notice]= "You do not have sufficient access privileges to read this document, which has been marked private."
+        redirect_to(:action => 'index', :q => nil , :f => nil) and return false
+      end
+    end
+  end
+
+
 end
 
