56f660322c126107db78a81f05cb9800b99359f3
Caroline Webb <caroline.webb@gcu.ac.uk>
2012-10-19 09:27:43 -0700

    Modified display resolution helper method to scale large videos to fitpage

--- config/initializers/gcu_config.rb
+++ config/initializers/gcu_config.rb
@@ -1,5 +1,5 @@
 # GCU specific configurations
 
-  raw_config = File.read(RAILS_ROOT + "/config/gcu.yml")
-  GCU_CONFIG = YAML.load(raw_config)[RAILS_ENV]
+  raw_config = File.read(Rails.root.to_s + "/config/gcu.yml")
+  GCU_CONFIG = YAML.load(raw_config)[Rails.env]
 
